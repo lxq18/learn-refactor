9292b7a8a538114671e4138c98abc81d17babc10
xqli <itxeli@126.com>
2019-08-03 17:42:45 +0800

    重构：Extract Method

---
+++ src/main/java/com/lxq/learn/refactor/ExtractMethod.java
@@ -0,0 +1,33 @@
+package com.lxq.learn.refactor;
+
+/**
+ * @author lxq
+ * @create 2019/8/3 17:36
+ */
+public class ExtractMethod {
+    public int getNum(int day) {
+        int result;
+        if (day > 20190803) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        return result;
+    }
+
+    public int getNum2(int day) {
+        int result;
+        result = calculateDay(day);
+        return result;
+    }
+
+    private int calculateDay(int day) {
+        int result;
+        if (day > 20190803) {
+            result = 1;
+        } else {
+            result = 0;
+        }
+        return result;
+    }
+}
---
+++ src/main/java/com/lxq/learn/refactor/domain/Category.java
@@ -0,0 +1,13 @@
+package com.lxq.learn.refactor.domain;
+
+import lombok.Data;
+
+/**
+ * @author lxq
+ * @create 2019/8/3 17:36
+ */
+@Data
+public class Category {
+    private Integer code;
+    private String name;
+}
